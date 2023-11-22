4fa6bb2e7108bec74a1b67475189e04baea14268
SidikR <rahmad.119140096@student.itera.ac.id>
2023-11-22 12:23:35 +0700

    update/halaman mail

--- resources/views/admin/layouts/partials/navbar.blade.php
+++ resources/views/admin/layouts/partials/navbar.blade.php
@@ -7,7 +7,7 @@
             <ul
                 class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
                 <li>
-                    <a href=""
+                    <a href="{{ route('webmail') }}"
                         class="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
                         aria-current="page"><i class="bi bi-envelope-at-fill pe-2"></i>Email</a>
                 </li>
