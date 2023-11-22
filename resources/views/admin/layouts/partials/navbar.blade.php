2f01cdedc5b6a5ef7cee36d53152f8c80d003292
SidikR <rahmad.119140096@student.itera.ac.id>
2023-11-22 12:32:03 +0700

    update link

--- resources/views/admin/layouts/partials/navbar.blade.php
+++ resources/views/admin/layouts/partials/navbar.blade.php
@@ -7,22 +7,10 @@
             <ul
                 class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
                 <li>
-                    <a href="{{ route('webmail') }}"
-                        class="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                        aria-current="page"><i class="bi bi-envelope-at-fill pe-2"></i>Email</a>
+                    <a href="/webmail" target="_blank"
+                        class="aria-current= block rounded py-2 pl-3 pr-4 text-black md:bg-transparent md:p-0"page"><i
+                            class="bi bi-envelope-at-fill pe-2"></i>Email</a>
                 </li>
-                {{-- <li>
-                    <a href="#"
-                        class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">About</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">Services</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500">Contact</a>
-                </li> --}}
             </ul>
         </div>
     </div>
